ee140816e9ba07016117288ae289fc37a217fb89
LinhaoLi <jerrylilinhao@gmail.com>
2017-09-20 03:30:58 +1000

    withGrade

--- Assignment1/Assignment.java
+++ Assignment1/Assignment.java
@@ -9,16 +9,29 @@ public class Assignment implements SubmissionHistory {
 	/**
 	 * Default constructor
 	 */
-	TreeMap<String, Details> students = new TreeMap<String, Details>();
+	public TreeMap<String, Details> students;
 
 	public Assignment() {
-		// TODO initialise your data structures
+		this.students = new TreeMap<String, Details>();
 	}
 
 	private class Handin implements Submission{
 		private String unikey;
 		private Date timestamp;
 		private Integer grade;
+
+		public Handin(){
+			unikey = null;
+			timestamp = null;
+			grade = null;
+		}
+
+		public Handin(String newUnikey, Date newDate, Integer newGrade){
+			unikey = newUnikey;
+			timestamp = newDate;
+			grade = newGrade;
+		}
+
 		public String getUnikey(){
 			return unikey;
 		}
@@ -28,32 +41,88 @@ public class Assignment implements SubmissionHistory {
 		public Integer getGrade(){
 			return grade;
 		}
+		public void setUnikey(String newUnikey){
+			unikey = newUnikey;
+		}
+		public void setTime(Date newDate){
+			timestamp = newDate;
+		}
+		public void setGrade(Integer newGrade){
+			grade = newGrade;
+		}
 	}
 
 
 	private class Details{
-		public Integer bestGrade;
+		public Details(){
+			bestGrade = -1;
+			latestGrade = -1;
+			latest = null;
+			sortedDates = null;
+			sortedGrades = null;
+		}
 		public Integer latestGrade;
-		public TreeMap<Date, String> myGrades;
+		public Integer bestGrade;
+		public Date latest;
+		public TreeMap<Date,  Handin> sortedDates;
+		public TreeMap<Integer, Handin> sortedGrades;
 	}
 
 
 	@Override
-	public Integer getBestGrade(String unikey) {
+	public Integer getBestGrade(String unikey) throws IllegalArgumentException{
+		if(unikey == null){
+			throw new IllegalArgumentException("The argument does not exist");
+		}
+		if(this.students.get(unikey) == null){
+			return null;
+		}
+		Details thisStudent = this.students.get(unikey);
+		if(thisStudent.bestGrade == -1){
+			return null;
+		}
+		else{
+			return thisStudent.bestGrade;
+		}
 		// TODO Implement this, ideally in better than O(n)
-		return null;
 	}
 
 	@Override
-	public Submission getSubmissionFinal(String unikey) {
+	public Submission getSubmissionFinal(String unikey) throws IllegalArgumentException{
+		if(unikey == null){
+			throw new IllegalArgumentException("The argument does not exist");
+		}
+		if(this.students.get(unikey) == null){
+			return null;
+		}
+		Details thisStudent = this.students.get(unikey);
+
+		if(thisStudent.latestGrade == -1){
+			return null;
+		}
+		else{
+			return thisStudent.sortedDates.get(thisStudent.latest);
+		}
 		// TODO Implement this, ideally in better than O(n)
-		return null;
 	}
 
 	@Override
-	public Submission getSubmissionBefore(String unikey, Date deadline) {
+	public Submission getSubmissionBefore(String unikey, Date deadline) throws IllegalArgumentException{
+		if(unikey == null||deadline == null){
+			throw new IllegalArgumentException("The argument does not exist");
+		}
+		if(this.students.get(unikey) == null){
+			return null;
+		}
+		Details thisStudent = this.students.get(unikey);
+
+		if(thisStudent.latestGrade == -1){
+			return null;
+		}
+		else{
+			return thisStudent.sortedDates.get(thisStudent.sortedDates.floorKey(deadline));
+		}
 		// TODO Implement this, ideally in better than O(n)
-		return null;
 	}
 
 	@Override
@@ -62,25 +131,44 @@ public class Assignment implements SubmissionHistory {
 		if(unikey == null || timestamp == null || grade == null){
 			return null;
 		}
+		Handin submission = new Handin(unikey, timestamp, grade);
 
-		SimpleDateFormat dt = new SimpleDateFormat("yymmddhhmmss");
-		String s = dt.format(timestamp);
-
-		if(students.get(unikey) == null){
-			TreeMap<Integer, String> tm1 = new TreeMap<Integer, String>();
-			tm1.put(grade, s);
-			students.put(unikey, tm1);
+		if(this.students.get(unikey) == null){
+			Details dts = new Details();
+			dts.bestGrade = grade;
+			dts.latestGrade = grade;
+			dts.latest = timestamp;
+			dts.sortedDates.put(timestamp, submission);
+			dts.sortedGrades.put(grade, submission);
+			this.students.put(unikey, dts);
 		}
 		else{
-			students.get(unikey).put(grade, s);
+			Details thisStudent = this.students.get(unikey);
+			if(thisStudent.bestGrade < grade){
+				thisStudent.bestGrade = grade;
+			}
+			if(timestamp.compareTo(thisStudent.latest) > 0){
+				thisStudent.latestGrade = grade;
+				thisStudent.latest = timestamp;
+			}
+			thisStudent.sortedDates.put(timestamp, submission);
+			thisStudent.sortedGrades.put(grade, submission);
 		}
-//idk
-		return null;
+		return submission;
 	}
 
 	@Override
-	public void remove(Submission submission) {
+	public void remove(Submission submission) throws IllegalArgumentException{
 		// TODO Implement this, ideally in better than O(n)
+		if(submission == null){
+			throw new IllegalArgumentException("The argument does not exist");
+		}
+
+		String removeUnikey = submission.getUnikey();
+		Date removeTimestamp = submission.getTime();
+		Integer removeGrade = submission.getGrade();
+
+
 
 
 
